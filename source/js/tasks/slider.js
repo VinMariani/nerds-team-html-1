4fd70ca9cd7014e03b8144e5d0fa2891e652d90b
Andrew Chernigovsky <chernigovsky108@gmail.com>
2024-02-08 21:23:12 +0300

    new features

--- source/js/tasks/slider.js
+++ source/js/tasks/slider.js
@@ -3,6 +3,31 @@
 // Переменные такого вида.
 // const slider = document.querySelector('.slider');
 
-export function slider() {
-	// Тут пишешь свой код
+// export function slider() {
+// 	// Тут пишешь свой код
+// }
+
+const data = [1, 2, 3, 4, 5];
+const data1 = ['name', 'city', 'village', 'age', 'gender'];
+let obj = {};
+let arr = [];
+
+data1.forEach((el) => {
+	obj.name = el;
+	arr.push(obj);
+})
+
+console.log(arr, 'arr');
+arr = [{
+	"name": 1,
+	"city": 2,
+	"village": 3,
+},
+{
+	"age": 5
+},
+{
+	"city": 4,
+	"name": 3
 }
+]
