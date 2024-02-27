345d36909ea72658e27b04ef8b66518332352e08
Tatiana B <tbeketova04@gmail.com>
2024-02-27 19:50:53 +0200

    переносит стили для модального окна в modal.scss

--- source/js/script.js
+++ source/js/script.js
@@ -1,19 +1,8 @@
-import {
-	slider
-}
-from "./tasks/slider.js";
-
-/* import {
-	modal
-}
-from "./tasks/modal.js"; */
-
+import { slider} from "./tasks/slider.js";
 
 document.addEventListener('DOMContentLoaded', () => {
 	slider();
-	//modal();
 })
-
 
 import { createModal } from "./tasks/modal.js";
 
