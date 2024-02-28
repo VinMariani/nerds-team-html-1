ead43a86baee0baa4ca0864f0c06c2109ffee106
Tatiana B <tbeketova04@gmail.com>
2024-02-28 09:13:05 +0200

    добавляет другой скрипт js для модального окна

--- source/js/tasks/modal.js
+++ source/js/tasks/modal.js
@@ -22,3 +22,30 @@ export function createModal() {
         });
     }
 }
+
+/* const modal = document.querySelector('.modal');
+
+export function createModal() {
+	if (modal) {
+		const overlay = document.getElementById('overlay');
+		const buttonCloseModal = document.querySelector('.modal__close-btn');
+		const buttonOpenModal = document.querySelector('.map-contacts__btn');
+		const ACTIVE = 'active';
+
+		const isShowModal = (el) => {
+			el.classList.toggle(ACTIVE);
+			overlay.classList.toggle(ACTIVE)
+		}
+
+		buttonOpenModal.onclick = () => isShowModal(modal);
+		buttonCloseModal.onclick = () => isShowModal(modal);
+		overlay.onclick = () => isShowModal(modal);
+
+		document.addEventListener('keyup', (e) => {
+			if (e.key === 'Escape') {
+				isShowModal(modal);
+				console.log(e.key, 'esc');
+			}
+		})
+	}
+} */
